e8d4f1e0026c79f0f0516a08ee302873f234148e
celitacherry <celitacherry@outlook.com>
2021-09-20 16:54:22 -0400

    added form input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -23,15 +23,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
- return(
- 
- <div>
-  <h2>Let's get started!</h2>
-  <Expenses items={expenses}/>
-  </div>
-  
-);
-  
+  return (
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
